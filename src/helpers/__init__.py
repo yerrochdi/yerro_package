674fdb1b05956223bf7d08560b76182da45116df
Errochdi Yassine <yerrochdi.telecomparis@gmail.com>
2022-06-03 10:03:42 +0200

    doocstring and shabang

--- src/helpers/__init__.py
+++ src/helpers/__init__.py
@@ -1,3 +1,7 @@
+"""
+Helpers is package that provides easy to use helper function,
+and variable.
+"""
 __all__=['extract_upper']
 
 from .strings import *
--- src/helpers/strings.py
+++ src/helpers/strings.py
@@ -1,6 +1,26 @@
 def extract_upper(phrase):
+    """_summary_
+    extract upper test
+    Args:
+        phrase (str): _description_
+
+    Returns:
+        list: Only upper case character from the string
+    >>> extract_upper("Hello Yassine")
+    ['H', 'Y']
+    """
     return list(filter(lambda letter: letter.isupper(), phrase))
 
 
 def extract_lower(phrase):
+    """_summary_
+    extract lower test
+    Args:
+        phrase (str): _description_
+
+    Returns:
+        list: Only lower case character from the string
+    >>> extract_lower("Hello Yassine")
+    ['e', 'l', 'l', 'o', 'a', 's', 's', 'i', 'n', 'e']
+    """
     return list(filter(lambda letter: letter.islower(), phrase))
